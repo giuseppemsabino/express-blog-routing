67b631eeab2fb7837220b3a892bcb9bf17bc3a20
giuseppemsabino <giuseppemendoza004@gmail.com>
2024-11-20 18:16:45 +0100

    delete complete

--- routers/posts.js
+++ routers/posts.js
@@ -14,15 +14,26 @@ router.get('/:id', (req, res) => {
 
 //store
 router.post('/', (req, res) => {
-    res.json(`crea un nuovo post`)
+    res.json(`Crea un nuovo post`)
 });
 
 //update
 router.put('/:id', (req,res) => {
     const {id}= req.params;
-    res.json(`modifica del post con id ${id}`);
+    res.json(`Modifica totale del post con id ${id}`);
 });
 
+//modify
+router.patch('/:id', (req,res)=> {
+    const {id}= req.params;
+    res.json(`Modifica parziale del post con id ${id}`);
+});
+
+//delete
+router.delete('/:id',  (req,res)=> {
+    const {id}= req.params;
+    res.json(`Eliminazione del post con id ${id}`);
+});
 
 
 
